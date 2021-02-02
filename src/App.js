4cf21f8e5c0a1ac689554b74a09558a3a669a459
fredrikasundell <70134993+FredrikaSundell@users.noreply.github.com>
2021-02-02 17:05:26 +0100

    navbar

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+import './App.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'Fredrika Sundell',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: 'Hej',
+        subTitle: 'Projekts',
+        text: 'Check out',
+      },
+      about: {
+        title: 'About me',
+      },
+      contact: {
+        title: 'Contact me',
+      },
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Fredrika Sundell</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    )
+  }
+}
+
+export default App
