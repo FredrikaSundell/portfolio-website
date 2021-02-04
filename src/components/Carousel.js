381d3d320fe6763e605abcfc5b3cffd14e973555
fredrikasundell <70134993+FredrikaSundell@users.noreply.github.com>
2021-02-04 17:11:31 +0100

    WIP Card and CardInfo components

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import Card from '../components/Card'
+
+import quire from '../assets/images/quire.png'
+import todo from '../assets/images/todo.png'
+import portfolio from '../assets/images/portfolio.png'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
 class Carousel extends React.Component {
   constructor(props) {
     super(props)
@@ -9,6 +18,7 @@ class Carousel extends React.Component {
           id: 0,
           title: 'Quire',
           subTitle: 'Notebook - a group project built for KYH Yrkeshögskola.',
+          imgSrc: quire,
           link:
             'https://github.com/Viktor-Hultman/quire_notebook_assignment_paperclip',
           selected: false,
@@ -16,7 +26,8 @@ class Carousel extends React.Component {
         {
           id: 1,
           title: 'Todo',
-          subTitle: 'Todo app - made with JavaScript.',
+          subTitle: 'An unstyled todo-app made with JavaScript.',
+          imgSrc: todo,
           link: 'https://fredrikasundell.github.io/todo-app/',
           selected: false,
         },
@@ -24,7 +35,8 @@ class Carousel extends React.Component {
           id: 2,
           title: 'Portfolio',
           subTitle:
-            'Portfolio - first project ever done (built for KYH Yrkeshögskola) with html, css and JavaScript.',
+            'Portfolio - first project ever done (built for KYH Yrkeshögskola) with HTLM, CSS and JavaScript.',
+          imgSrc: portfolio,
           link: 'https://fredrikasundell.github.io/IP1/',
           selected: false,
         },
@@ -32,8 +44,42 @@ class Carousel extends React.Component {
     }
   }
 
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items]
+
+    items[id].selected = items[id].selected ? false : true
+
+    items.forEach((item) => {
+      if (item.id !== id) {
+        item.selected = false
+      }
+    })
+
+    this.setState({
+      items,
+    })
+  }
+
+  makeItems = (items) => {
+    return items.map(item => {
+      return (
+        <Card
+          item={item}
+          onClick={(e => this.handleCardClick(item.id, e))}
+          key={item.id}
+        />
+      )
+    })
+  }
+
   render() {
-    return <p>Carousel Works!</p>
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-around">
+          {this.makeItems(this.state.items)}
+        </Row>
+      </Container>
+    )
   }
 }
 
