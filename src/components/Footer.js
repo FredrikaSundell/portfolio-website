2d02b565a16794c6a125e23fbcb883e98ae36359
fredrikasundell <70134993+FredrikaSundell@users.noreply.github.com>
2021-02-09 19:03:53 +0100

    deleted junk  and add API Weather Sthlm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import WeatherHook from './WeatherHook'
 
 function Footer() {
   return (
     <footer className="mt-5">
       <Container fluid={true}>
         <Row className="border-top justify-content-between p-3">
-          <Col className="p-0" md={3} sm={12}>
-            Fredrika Sundell
-          </Col>
+
+          <WeatherHook />
           <Col className="p-0 d-flex justify-content-end" md={3}>
             Made by Fredrika Sundell.
           </Col>
---
+++ src/components/WeatherHook.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+const url =
+  'http://api.openweathermap.org/data/2.5/weather?q=stockholm,se&APPID=f8384513fad5f91ea04d07a2cbf916ec&units=metric'
+
+function WeatherHook() {
+  const [data, setData] = useState({ data: null })
+
+  useEffect(() => {
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setData({ data: json }))
+  }, [])
+
+  return (
+    <span>
+      Feels like {!data.data ? '?' : data.data.main.feels_like.toFixed(1)}°C in
+      Stockholm right now
+    </span>
+  )
+}
+
+export default WeatherHook
